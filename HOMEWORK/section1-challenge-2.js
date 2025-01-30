879ee648ef29d3fb0345d6e7fbfeebf7fdb59eb9
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-01-30 16:56:03 +0200

    update

---
+++ HOMEWORK/section1-challenge-2.js
@@ -0,0 +1,6 @@
+const cities = ["Athens", "London", "Paris"]
+cities.push("Berlin")
+
+for(const city of cities){
+  console.log(city)
+}
---
+++ HOMEWORK/section1-challenge1.js
@@ -0,0 +1,23 @@
+
+let userName = "Marios"
+console.log(`Hello ${userName}`)
+
+
+const calc = {
+  add: function (a, b) {
+    return a + b
+  }
+}
+
+let num1 = 10
+let num2 = 10
+
+result = num1 + num2
+console.log(result)
+
+console.log(calc.add(1, 5))
+
+
+let isSnowing = false;
+let message = isSnowing ? "Είναι αλήθεια!" : "Είναι ψευδές!";
+console.log(message);
